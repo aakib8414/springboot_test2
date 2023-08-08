aa37800ab383fdb81b46f3417d332db6cefe4c93
mohd aakib <aakib@binmile.com>
2023-08-08 18:04:56 +0530

    file and json handling together

--- src/main/java/com/test/test1/controller/FileController.java
+++ src/main/java/com/test/test1/controller/FileController.java
@@ -1,8 +1,13 @@
 package com.test.test1.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.test.test1.batch.model.User;
 import com.test.test1.service.file.FileResponse;
 import com.test.test1.service.file.FileService;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -25,20 +30,30 @@ import java.io.InputStream;
 @RequestMapping("/file")
 public class FileController {
 
+    private static Logger logger = LoggerFactory.getLogger(FileController.class);
     @Autowired
     private FileService fileService;
 
+    @Autowired
+    private ObjectMapper mapper;
     @Value("${project.image}")
     private String path;
     @PostMapping("/upload")
-    public ResponseEntity<FileResponse> fileUpload(@RequestParam("image")MultipartFile image)  {
+    public ResponseEntity<FileResponse> fileUpload(@RequestParam("image")MultipartFile image,
+    @RequestParam("user") String user) throws JsonProcessingException {
         String fileName = null;
         try {
             fileName = fileService.uploadImage(path, image);
         } catch (IOException e) {
             return new ResponseEntity<>(new FileResponse(fileName,"Image uploaded failed due to internal errors"), HttpStatus.INTERNAL_SERVER_ERROR);
         }
+        convertStringToJson(user);
         return new ResponseEntity<>(new FileResponse(fileName,"Uploaded successfully"), HttpStatus.OK);
+    }
+
+    private void convertStringToJson(String user) throws JsonProcessingException {
+        User jsonUser = mapper.readValue(user, User.class);
+        logger.info("JSON USER:{}",jsonUser);
     }
 
     //method serve image
