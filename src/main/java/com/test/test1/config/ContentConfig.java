131f98d386228fa007614b9110e5b8035e91f6db
mohd aakib <aakib@binmile.com>
2023-08-08 17:44:47 +0530

    ch

--- src/main/java/com/test/test1/config/ContentConfig.java
+++ src/main/java/com/test/test1/config/ContentConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class ContentConfig implements WebMvcConfigurer {
 
